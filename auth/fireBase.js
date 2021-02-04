8d43f1f1297d2070cdc89eda66475346f720bc00
mahirmolai27 <69717367+mahirmolai27@users.noreply.github.com>
2021-02-04 21:41:18 +0500

    Add Login System...
    New Login System Added

---
+++ auth/fireBase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB6xFxBARKIHRRsod0Qd4mLWDsuHKpWL9M",
+    authDomain: "scos-a8a42.firebaseapp.com",
+    projectId: "scos-a8a42",
+    storageBucket: "scos-a8a42.appspot.com",
+    messagingSenderId: "1077321501162",
+    appId: "1:1077321501162:web:a4073143ddf35960908a93",
+    measurementId: "G-RZPE6BRTVK"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
